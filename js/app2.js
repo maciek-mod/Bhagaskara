19ec7154bfa78ff3967a44c0eb6a888df325df65
maciek-mod <maciek.modlinski1988@gmail.com>
2017-01-18 10:01:11 +0100

    section team slider

--- js/app2.js
+++ js/app2.js
@@ -111,6 +111,27 @@ $(function(){
   });
 
 
+  $('#myCarousel').carousel({
+    interval: 10000
+  })
+  //team slider
+  if ($(window).width() >= 768) {
+
+    $('.carousel .item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      if (next.next().length>0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+      }
+      else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+      }
+    });
+  }
 
 
 });
